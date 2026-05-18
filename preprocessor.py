a6a7f1df6f4b6f35c91123c2d2c89fe374558896
anshiku <anastasiiashiku285@gmail.com>
2026-05-18 14:56:15 +0300

    лаб1-препроцессор-Шикунова

--- preprocessor.py
+++ preprocessor.py
@@ -23,3 +23,47 @@ def preprocess(text):
             lines.append(line)
 
     return "\n".join(lines)
+
+def main():
+
+    input_file = "test.cpp"
+
+    output_file = "cleaned_test.cpp"
+
+ 
+    try:
+
+        code = read_file(input_file)
+
+ 
+        if has_unclosed_comment(code):
+
+            print("Ошибка: незакрытый многострочный комментарий.")
+
+            return
+
+ 
+        cleaned = preprocess(code)
+
+        write_file(output_file, cleaned)
+
+ 
+        print("Файл обработан успешно.")
+
+        print(f"Результат сохранён в {output_file}")
+
+        print("Ошибок не выявлено.")
+
+ 
+    except FileNotFoundError:
+
+        print(f"Ошибка: файл {input_file} не найден.")
+
+    except Exception as e:
+
+        print(f"Ошибка: {e}")
+
+ 
+if __name__ == "__main__":
+
+    main()
